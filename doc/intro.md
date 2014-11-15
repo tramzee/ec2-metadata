849c06ebe7a51522626f4037db956272b32f3e42
Tim Ramsey <tramsey@gmail.com>
2014-11-15 12:39:57 -0500

    Setting up the repo

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to ec2-metadata
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/io/hivewing/ec2_metadata_test.clj
@@ -0,0 +1,7 @@
+(ns io.hivewing.ec2-metadata-test
+  (:require [clojure.test :refer :all]
+            [io.hivewing.ec2-metadata :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
