134882b3aaf7618ec16868419e27eb11145626dc
Tim Ramsey <tramsey@gmail.com>
2014-11-15 15:56:57 -0500

    Real functionality

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject io.hivewing/ec2-metadata "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A Simple lib for accessing metadata on AWS EC2"
+  :url "http://github.com/tramzee/ec2-metadata.git"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :aot :all
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/data.json "0.2.5"]])
--- src/io/hivewing/ec2_metadata.clj
+++ src/io/hivewing/ec2_metadata.clj
@@ -1,6 +1,24 @@
-(ns io.hivewing.ec2-metadata)
+(ns io.hivewing.ec2-metadata
+  (require [clojure.data.json :as json]
+           [clojure.java.io :as io ]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn- category-data-stream [cat]
+  (let [target (str "http://169.254.169.254/latest/meta-data/" cat)]
+    (io/reader target)))
+
+(defn- key-fn [k]
+  (->> (clojure.string/split k #"(?<=[a-z])(?=[A-Z])")
+       (clojure.string/join \-)
+       clojure.string/lower-case
+       keyword))
+
+(defn metadata
+  "Return a map of category data from the instance"
+  [category]
+  (try
+    (with-open [j (category-data-stream category)]
+      (let [r (slurp j)]
+        (if (first r \{)
+          (json/read-str r :key-fn key-fn)
+          r)))
+    (catch Exception e)))
