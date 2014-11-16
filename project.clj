ce64918b983f58d929a95e86fcc79aaa2b41e293
Tim Ramsey <tramsey@gmail.com>
2014-11-16 11:04:49 -0500

    Official 0.1.0 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.hivewing/ec2-metadata "0.1.0-SNAPSHOT"
+(defproject io.hivewing/ec2-metadata "0.1.0"
   :description "A Simple lib for accessing metadata on AWS EC2"
   :url "http://github.com/tramzee/ec2-metadata.git"
   :license {:name "Eclipse Public License"
